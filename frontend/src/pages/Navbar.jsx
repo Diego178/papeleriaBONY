7b9725ce65c04cbb6162532897960994663a9811
Josué David Martínez Gutiérrez <47648280+JosueDavidMartz@users.noreply.github.com>
2023-04-19 09:06:26 -0500

    commitRegistro

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -5,6 +5,7 @@ const imagen = require.context('../assets/navbar/', true);
 
 const Navbar = () => {
   return (
+
       <nav className="bg-gray-800 sticky">
         <div className="relative flex h-17 items-center justify-between">
           <Link to='/Inicio'>
@@ -30,7 +31,7 @@ const Navbar = () => {
             <div className="flex-1 align-content: flex-end">
               <div className="flex justify-end space-x-4">
                 <Link className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page" to='/Registrarse'>Registrarse</Link>
-                <Link className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page" to='/Login'>Login</Link>
+                <Link className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page" to='/Login'>Iniciar sesión</Link>
                 <Link to='/Carrito'>
                   <img src={imagen(`./carrito.png`)}width="50"/>
                 </Link>
--- frontend/src/pages/Registrarse.jsx
+++ frontend/src/pages/Registrarse.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+
+
+import React, { useState } from 'react';
+import '../registro.css';
 
 const Registrarse = () => {
-  return (
-    <div>Registrarse</div>
-  )
-}
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const [confirmarContrasena, setConfirmarContrasena] = useState('');
 
-export default Registrarse
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos del formulario a un servidor o base de datos
+    console.log('Formulario enviado:', { nombre, email, contrasena, confirmarContrasena });
+  };
+  
+
+  return (
+
+    <form className="formulario">
+      <h1>Regístrate</h1>
+      <div className="contenedor">
+        <div className="input-contenedor">
+          <i className="fas fa-user icon"></i>
+          <input type="text" placeholder="Nombre Completo" />
+        </div>
+        <div className="input-contenedor">
+          <i className="fas fa-envelope icon"></i>
+          <input type="text" placeholder="Correo Electrónico" />
+        </div>
+        <div className="input-contenedor">
+          <i className="fas fa-key icon"></i>
+          <input type="password" placeholder="Contraseña" id='contraRegistro' />
+        </div>
+        <input type="submit" value="Regístrate" className="button" />
+        <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
+        <p>¿Ya tienes una cuenta? <a className="link" href="loginvista.html">Iniciar Sesión</a></p>
+      </div>
+    </form>
+  );
+};
+
+export default Registrarse;
+
+
